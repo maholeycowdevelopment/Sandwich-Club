29e0dac763c1ce6951173b389d8dbc7fd4d7eafa
Patrick VanVorce <maholeycowdevelopment@gmail.com>
2018-04-25 17:34:30 -0700

    Created version 1 of parseSandwichJson inside of JsonUtils

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,11 +2,21 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
 /**
  * Utility class for parsing Json data
  */
 public class JsonUtils {
-
+//    private String mainName;
+//    private List<String> alsoKnownAs = null;
+//    private String placeOfOrigin;
+//    private String description;
+//    private String image;
+//    private List<String> ingredients = null;
     /**
      * Returns a Sandwich object to be rendered to the user
      * inside of DetailActivity. The json string comes from the
@@ -15,10 +25,20 @@ public class JsonUtils {
      * @return
      */
     public static Sandwich parseSandwichJson(String json) {
-        // TODO 1: Get mainName, aka, placeOfOrigin, description, image, ingredients and store in variables
+        try {
+            JSONObject jsonReader = new JSONObject(json);
+            String mainName = jsonReader.getJSONObject("name").getString("mainName");
+            List<String> alsoKnownAs = (List<String>) jsonReader.getJSONObject("name").getJSONArray("alsoKnowAs");
+            String placeOfOrigin = jsonReader.getString("placeOfOrigin");
+            String description = jsonReader.getString("description");
+            String image = jsonReader.getString("image");
+            List<String> ingredients = (List<String>) jsonReader.getJSONArray("ingredients");
 
-        //TODO 2: Create a new Sandwich object initialized with the variable from todo and return it
-        
-        return null;
+            Sandwich sandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+            return sandwich;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
