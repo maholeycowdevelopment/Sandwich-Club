169abe48973a44cbfc08353e88993e82f1093059
Patrick VanVorce <maholeycowdevelopment@gmail.com>
2018-04-25 17:12:13 -0700

    Added descriptive comments to JsonUtils.java

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,20 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+/**
+ * Utility class for parsing Json data
+ */
 public class JsonUtils {
 
+    /**
+     * Returns a Sandwich object to be rendered to the user
+     * inside of DetailActivity. The json string comes from the
+     * strings.xml file found in the res/values folder.
+     * @param json an string with info about a user chosen sandwich
+     * @return
+     */
     public static Sandwich parseSandwichJson(String json) {
+
         return null;
     }
 }
